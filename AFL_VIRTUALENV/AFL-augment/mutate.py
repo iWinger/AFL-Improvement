f9a7578123406a35a8f77b674e7aae7df0afbbce
iWinger <iwinger@LAPTOP-4VTC1IGL.localdomain>
2020-03-02 18:40:06 -0500

    python packages installed in virtual environment & angr installed for symbolic execution

---
+++ AFL_VIRTUALENV/AFL-augment/mutate.py
@@ -0,0 +1,18 @@
+#This is a python file that should recursively check through the files supplied by the argument and then modify seed selection
+
+import os
+import sys
+import angr # suite of python libraries for symbolic execution
+
+
+class Mutate(object):
+    ''' This is the class for mutating seeds '''
+    def __init__(self, input_str, fuzz_bitmap, argv=None):
+
+        self.input_str = input_str
+        self.fuzz_bitmap = fuzz_bitmap
+        self.argv = argv
+
+   # def mutate(self):
+            
+
